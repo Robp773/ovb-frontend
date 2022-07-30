ff9381e33c786188221a263799321475f6596627
Robert Peterman <robpeterman573@gmail.com>
2022-07-29 22:22:51 -0400

    Moved CartPtrovider HOC to only wrap store to avoid conflicts with other HOC libraries
    - added success alert after store checkout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,23 +1,12 @@
 import React from "react";
-import { CartProvider } from "use-shopping-cart";
 import Footer from "./footer";
 import NavBar from "./navbar";
 
 const Layout = ({ children }) => (
   <>
-    <CartProvider
-      mode="payment"
-      cartMode="client-only"
-      stripe={process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}
-      successUrl={`${process.env.GATSBY_BASE_URL}/store?status=success`}
-      cancelUrl={`${process.env.GATSBY_BASE_URL}/store`}
-      currency="USD"
-      allowedCountries={["US", "GB", "CA"]}
-    >
-      <NavBar />
-      {children}
-      <Footer />
-    </CartProvider>
+    <NavBar />
+    {children}
+    <Footer />
   </>
 );
 
--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -6,26 +6,39 @@ import Products from "../../components/products/products";
 import Cart from "../../components/products/cart-overview";
 import queryString from "query-string";
 import { Alert, Typography } from "@mui/material";
+import { CartProvider } from "use-shopping-cart";
 
 const StorePage = (data) => {
   const seo = { title: "Store" };
 
-  const queriedTheme = queryString.parse(data.location.search);
-  const { status } = queriedTheme;
+  const urlQuery = queryString.parse(data.location.search);
+  const { status } = urlQuery;
   return (
     <Layout>
-      <SEO seo={seo} />
-      <ContentWrapper>
-        {/* <Typography align="center" variant="h3">
-          OVB Store
-        </Typography> */}
-        <Alert severity="warning">
-          Store is currently being tested and real payments are disabled.
-        </Alert>
-
-        <Products />
-        <Cart status={status} />
-      </ContentWrapper>
+      <CartProvider
+        mode="payment"
+        cartMode="client-only"
+        stripe={process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}
+        successUrl={`${process.env.GATSBY_BASE_URL}/store?status=success`}
+        cancelUrl={`${process.env.GATSBY_BASE_URL}/store`}
+        currency="USD"
+        allowedCountries={["US", "GB", "CA"]}
+      >
+        <SEO seo={seo} />
+        <ContentWrapper>
+          <Alert sx={{ mb: 1 }} severity="warning">
+            Store is currently being tested and real payments are disabled.
+          </Alert>
+          {status === "success" && (
+            <Alert sx={{ mb: 1 }} severity="success">
+              Your order has been successfully submitted. Items will be brought
+              to the next practice or game.
+            </Alert>
+          )}
+          <Products />
+          <Cart status={status} />
+        </ContentWrapper>
+      </CartProvider>
     </Layout>
   );
 };
